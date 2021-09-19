e3a5885339e6a933f499565b09f75f61ec8089a4
Anshuman Bhaduri <anshuman@bhadurian.com>
2021-09-19 16:53:39 +1200

    Update extensions for lint-staged

---
+++ packages/mrm-preset/lint-staged/index.js
@@ -0,0 +1,27 @@
+const lintStaged = require('mrm-task-lint-staged');
+const { lines } = require('mrm-core');
+
+const task = () => {
+    // For every run of lint-staged, it appends "npx lint-staged" to pre-commit.
+    // This needs to happen only once and on subsequent runs the file should
+    // remain untouched
+    const preCommit = lines('.husky/pre-commit');
+    if (preCommit.exists()) {
+        preCommit.remove('npx lint-staged').save();
+    }
+
+    lintStaged({
+        lintStagedRules: {
+            prettier: {
+                extensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'html', 'css'],
+            },
+            eslint: {
+                extensions: ['js', 'jsx', 'ts', 'tsx'],
+            },
+        },
+    });
+};
+
+task.description = 'Configure lint-staged';
+
+module.exports = task;
