339238c5d9784cbc65445137d8b0b8b529ec5e3f
Anshuman Bhaduri <anshuman@bhadurian.com>
2021-09-21 01:02:43 +1200

    feat(mrm-preset): add commitizen configuration

--- packages/mrm-preset/commitlint/index.js
+++ packages/mrm-preset/commitlint/index.js
@@ -1,29 +1,63 @@
 const { spawnSync } = require('child_process');
+const { normalize } = require('path');
 const { install, lines, json, packageJson } = require('mrm-core');
 
-const packages = {
-    '@commitlint/config-conventional': '^13',
-    '@commitlint/cli': '^13',
+const setHook = (hook, script) => {
+    const isHuskyInstalled = !!packageJson().get('devDependencies.husky');
+
+    if (!isHuskyInstalled) {
+        return;
+    }
+
+    const hookPath = normalize(`.husky/${hook}`);
+    const hookFile = lines(hookPath);
+
+    if (hookFile.exists()) {
+        hookFile.add(script).save();
+    } else {
+        spawnSync('npx', ['husky', 'add', hookPath, script]);
+    }
 };
 
-const task = () => {
+const configureCommitlint = () => {
+    const packages = {
+        '@commitlint/config-conventional': '^13',
+        '@commitlint/cli': '^13',
+    };
+
     json('.commitlintrc.json', {
         extends: ['@commitlint/config-conventional'],
     }).save();
 
-    const isHuskyInstalled = !!packageJson().get('devDependencies.husky');
-    if (isHuskyInstalled) {
-        const hook = lines('.husky/commit-msg');
-        const hookScript = 'npx --no-install commitlint --edit';
-
-        if (hook.exists()) {
-            hook.add(hookScript).save();
-        } else {
-            spawnSync('npx', ['husky', 'add', '.husky/commit-msg', hookScript]);
-        }
-    }
+    setHook('commit-msg', 'npx --no-install commitlint --edit');
 
     install(packages);
+};
+
+const configureCommitizen = () => {
+    const packages = {
+        commitizen: '^4',
+        '@commitlint/cz-commitlint': '^13',
+    };
+
+    json('.czrc', {
+        path: '@commitlint/cz-commitlint',
+    }).save();
+
+    packageJson().setScript('cz', 'cz').save();
+
+    // So commitizen runs on git commit
+    setHook(
+        'prepare-commit-msg',
+        'exec < /dev/tty && node_modules/.bin/cz --hook || true',
+    );
+
+    install(packages);
+};
+
+const task = () => {
+    configureCommitlint();
+    configureCommitizen();
 };
 
 task.description = 'Configure Commitlint';
