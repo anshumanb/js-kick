8860c69517ad556e5ab688864e5a12d5b9852c41
Anshuman Bhaduri <anshuman@bhadurian.com>
2021-10-16 14:03:07 +1300

    feat(create): add project bootstrapping using --root option

--- packages/create/README.md
+++ packages/create/README.md
@@ -5,9 +5,35 @@ Anshuman's package initializer.
 ## Usage
 
 ```bash
-$ npm init @bhadurian <package-name> [--public]
+$ npm init @bhadurian <package-name> [[--root] [--public]]
 ```
+
+This will also set up a script to run MRM to set up the package/project further.
 
 ### Options
 
-`--public`: Sets `publishConfig.access` to `true`
+`--root`
+Use when setting up a project initially. This will
+
+-   Add a basic `.nvmrc`
+-   Add a basic `.editorconfig` for MRM
+-   Install MRM as a dev dependency
+
+`--public`
+Sets `publishConfig.access` to `true`, otherwise sets `private` to `true`.
+
+### Examples
+
+When at the root of your project:
+
+```bash
+$ npm init @bhadurian <project-name> --root
+$ npm run mrm <task-name>
+```
+
+When creating a new package in a monorepo:
+
+```bash
+$ npm init -w <package-path> @bhadurian <package-name>
+$ npm run -w <package-path> mrm <task-name>
+```
--- packages/create/create.js
+++ packages/create/create.js
@@ -1,13 +1,23 @@
 #!/usr/bin/env node
 
 const path = require('path');
+const { spawnSync } = require('child_process');
 const { writeFileSync } = require('fs');
 
-const writeJson = (filePath, data) =>
-    writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf-8');
+const writeFile = (filePath, data) =>
+    writeFileSync(filePath, `${data}\n`, 'utf-8');
 
-const main = (name, isPublic) => {
-    writeJson(path.resolve('./package.json'), {
+const writeJson = (filePath, data) =>
+    writeFile(filePath, JSON.stringify(data, null, 2));
+
+const editorConfigContents = `root = true
+
+[*]
+end_of_line = lf
+insert_final_newline = true`;
+
+const main = (name, { isPublic = false, isRoot = false }) => {
+    writeJson(path.resolve('package.json'), {
         name,
         version: '1.0.0',
         scripts: {
@@ -17,19 +27,48 @@ const main = (name, isPublic) => {
             ? { publishConfig: { access: 'public' } }
             : { private: true }),
     });
+
+    if (isRoot) {
+        writeFile('.nvmrc', 'lts/*');
+        // MRM itself uses .editorconfig for new files
+        writeFile('.editorconfig', editorConfigContents);
+        // Install mrm so subsequent mrm runs won't download the package. Also
+        // updates to config will not requires mrm downloads this way
+        // eslint-disable-next-line no-console
+        console.log('Installing mrm...');
+        spawnSync('npm', ['install', '--save-dev', 'mrm']);
+    }
+};
+
+const availableOptions = {
+    '--public': 'isPublic',
+    '--root': 'isRoot',
 };
 
 if (require.main === module) {
     const args = process.argv.slice(2);
-    const positional = args.filter((arg) => !arg.startsWith('-'));
-    const options = args.filter((arg) => arg.startsWith('-'));
-    const isPublic = options.includes('--public');
 
-    if (positional.length === 1 && (!options.length || isPublic)) {
-        main(positional[0], isPublic);
+    // Parse command line args without a library to keep this package dependency-free
+    const positional = args.filter((arg) => !arg.startsWith('-'));
+    const hasValidPositional = positional.length === 1 && !!positional[0];
+
+    const options = args.filter((arg) => arg.startsWith('-'));
+    const validOptions = Object.keys(availableOptions);
+    const hasValidOptions = options.every((option) =>
+        validOptions.includes(option),
+    );
+
+    if (hasValidPositional && hasValidOptions) {
+        const optionsObj = options.reduce((acc, option) => {
+            acc[availableOptions[option]] = true;
+            return acc;
+        }, {});
+        main(positional[0], optionsObj);
     } else {
         // eslint-disable-next-line no-console
-        console.error('Usage: npm init @bhadurian/create <name> [--public]');
+        console.error(
+            'Usage: npm init @bhadurian/create <name> [[--public] [--root]]',
+        );
         process.exitCode = 1;
     }
 }
