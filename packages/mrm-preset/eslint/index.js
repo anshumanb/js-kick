d067e7f3123de49ed82df98dcd5208ab1d7d3937
Anshuman Bhaduri <anshuman@bhadurian.com>
2021-11-20 20:21:23 +1300

    fix(mrm-preset): add missing linting script

--- packages/mrm-preset/eslint/index.js
+++ packages/mrm-preset/eslint/index.js
@@ -1,4 +1,4 @@
-const { install, lines, json, uninstall } = require('mrm-core');
+const { packageJson, install, lines, json, uninstall } = require('mrm-core');
 const { isInstalled } = require('../utils');
 
 const task = () => {
@@ -28,6 +28,8 @@ const task = () => {
     if (!isInstalled('eslint-config-airbnb')) {
         install({ 'eslint-config-airbnb-base': '^14.2.1' });
     }
+
+    packageJson().setScript('lint', 'eslint --cache .').save();
 };
 
 task.description = 'Configure Prettier';
